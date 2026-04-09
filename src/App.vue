4ea9016a9d151eac1b0a797e1f2b4092a5983c4a
Liu jiale <liujiale2024@163.com>
2026-04-09 15:57:19 +1200

    fix src tracking

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>CS732 Vue Tutorial</h1>
+    <p>This is my Vue project</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+</style>
